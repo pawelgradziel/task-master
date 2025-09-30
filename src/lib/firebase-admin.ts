f9bd6346adcb3a039422489a77b23184f24c14e4
Paweł Grądziel <pawel.gradziel@fingo.net>
2025-09-30 13:55:25 +0200

    allow private/service-account.json to be missing

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,21 @@
 import admin from 'firebase-admin';
 
-const serviceAccountConfig = require('../../private/service-account.json')
+let credential;
+
+try {
+  // Try to load service account from file
+  const serviceAccountConfig = require('../../private/service-account.json');
+  credential = admin.credential.cert(serviceAccountConfig);
+} catch (error) {
+  // If service account file doesn't exist, try to use default credentials
+  // This works in environments like Google Cloud Run, App Engine, etc.
+  console.log('Service account file not found, using default credentials');
+  credential = admin.credential.applicationDefault();
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountConfig)
+    credential
   })
 }
 
